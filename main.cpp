d4d5ed5ea51c16ee078e1e88430dc8934a55d4ef
Juraj <37395233+JurajX@users.noreply.github.com>
2022-07-29 20:29:05 +0200

    beautify

--- main.cpp
+++ main.cpp
@@ -27,7 +27,7 @@
  * Helper to return the current time in milliseconds.
  */
 static unsigned millis() {
-	return static_cast<unsigned>((clock() * 1000LL) / CLOCKS_PER_SEC);
+    return static_cast<unsigned>((clock() * 1000LL) / CLOCKS_PER_SEC);
 }
 
 /**
@@ -41,15 +41,15 @@ static const etc1_to_dxt1_56_solution known[32 * 8 * NUM_ETC1_TO_DXT1_SELECTOR_M
  * Helper to compare two tables to see if they match.
  */
 static bool verifyTable(const etc1_to_dxt1_56_solution* a, const etc1_to_dxt1_56_solution* b) {
-	for (unsigned n = 0; n < 32 * 8 * NUM_ETC1_TO_DXT1_SELECTOR_MAPPINGS * NUM_ETC1_TO_DXT1_SELECTOR_RANGES; n++) {
-		if (a->m_hi != b->m_hi || a->m_lo != b->m_lo || a->m_err != b->m_err) {
-			printf("Failed with n = %d\n", n);
-			return false;
-		}
-		a++;
-		b++;
-	}
-	return true;
+    for (unsigned n = 0; n < 32 * 8 * NUM_ETC1_TO_DXT1_SELECTOR_MAPPINGS * NUM_ETC1_TO_DXT1_SELECTOR_RANGES; n++) {
+        if (a->m_hi != b->m_hi || a->m_lo != b->m_lo || a->m_err != b->m_err) {
+            printf("Failed with n = %d\n", n);
+            return false;
+        }
+        a++;
+        b++;
+    }
+    return true;
 }
 
 //************************ Optimisation Task Goes Here ************************/
@@ -64,65 +64,46 @@ static etc1_to_dxt1_56_solution result[32 * 8 * NUM_ETC1_TO_DXT1_SELECTOR_MAPPIN
  */
 static void create_etc1_to_dxt1_6_conversion_table()
 {
-	uint32_t n = 0;
+    uint32_t n = 0;
+    for (int inten = 0; inten < 8; inten++) {
+        for (uint32_t g = 0; g < 32; g++) {
+            color32 block_colors[4];
+            decoder_etc_block::get_diff_subblock_colors(block_colors, decoder_etc_block::pack_color5(color32(g, g, g, 255), false), inten);
+            for (uint32_t sr = 0; sr < NUM_ETC1_TO_DXT1_SELECTOR_RANGES; sr++) {
+                const uint32_t low_selector = g_etc1_to_dxt1_selector_ranges[sr].m_low;
+                const uint32_t high_selector = g_etc1_to_dxt1_selector_ranges[sr].m_high;
+                for (uint32_t m = 0; m < NUM_ETC1_TO_DXT1_SELECTOR_MAPPINGS; m++) {
+                    uint32_t best_lo = 0;
+                    uint32_t best_hi = 0;
+                    uint32_t best_err = UINT32_MAX;
+                    for (uint32_t hi = 0; hi <= 63; hi++) {
+                        for (uint32_t lo = 0; lo <= 63; lo++) {
+                            uint32_t colors[4];
 
-	for (int inten = 0; inten < 8; inten++)
-	{
-		for (uint32_t g = 0; g < 32; g++)
-		{
-			color32 block_colors[4];
-			decoder_etc_block::get_diff_subblock_colors(block_colors, decoder_etc_block::pack_color5(color32(g, g, g, 255), false), inten);
+                            colors[0] = (lo << 2) | (lo >> 4);
+                            colors[3] = (hi << 2) | (hi >> 4);
+                            colors[1] = (colors[0] * 2 + colors[3]) / 3;
+                            colors[2] = (colors[3] * 2 + colors[0]) / 3;
 
-			for (uint32_t sr = 0; sr < NUM_ETC1_TO_DXT1_SELECTOR_RANGES; sr++)
-			{
-				const uint32_t low_selector = g_etc1_to_dxt1_selector_ranges[sr].m_low;
-				const uint32_t high_selector = g_etc1_to_dxt1_selector_ranges[sr].m_high;
-
-				for (uint32_t m = 0; m < NUM_ETC1_TO_DXT1_SELECTOR_MAPPINGS; m++)
-				{
-					uint32_t best_lo = 0;
-					uint32_t best_hi = 0;
-					uint32_t best_err = UINT32_MAX;
-
-					for (uint32_t hi = 0; hi <= 63; hi++)
-					{
-						for (uint32_t lo = 0; lo <= 63; lo++)
-						{
-							uint32_t colors[4];
-
-							colors[0] = (lo << 2) | (lo >> 4);
-							colors[3] = (hi << 2) | (hi >> 4);
-
-							colors[1] = (colors[0] * 2 + colors[3]) / 3;
-							colors[2] = (colors[3] * 2 + colors[0]) / 3;
-
-							uint32_t total_err = 0;
-
-							for (uint32_t s = low_selector; s <= high_selector; s++)
-							{
-								int err = block_colors[s].g - colors[g_etc1_to_dxt1_selector_mappings[m][s]];
-
-								total_err += err * err;
-							}
-
-							if (total_err < best_err)
-							{
-								best_err = total_err;
-								best_lo = lo;
-								best_hi = hi;
-							}
-						}
-					}
-
-					assert(best_err <= 0xFFFF);
-
-					result[n] = (etc1_to_dxt1_56_solution){ (uint8_t)best_lo, (uint8_t)best_hi, (uint16_t)best_err };
-
-					n++;
-				} // m
-			} // sr
-		} // g
-	} // inten
+                            uint32_t total_err = 0;
+                            for (uint32_t s = low_selector; s <= high_selector; s++) {
+                                int err = block_colors[s].g - colors[g_etc1_to_dxt1_selector_mappings[m][s]];
+                                total_err += err * err;
+                            }
+                            if (total_err < best_err) {
+                                best_err = total_err;
+                                best_lo = lo;
+                                best_hi = hi;
+                            }
+                        }
+                    }
+                    assert(best_err <= 0xFFFF);
+                    result[n] = (etc1_to_dxt1_56_solution){ (uint8_t)best_lo, (uint8_t)best_hi, (uint16_t)best_err };
+                    n++;
+                } // m
+            } // sr
+        } // g
+    } // inten
 }
 
 //******************************** Entry Point ********************************/
@@ -131,23 +112,23 @@ static void create_etc1_to_dxt1_6_conversion_table()
  * Tests the generation and benchmarks it.
  */
 int main(int /*argc*/, char* /*argv*/[]) {
-	// Run this once and compare the result to the known table
-	create_etc1_to_dxt1_6_conversion_table();
-	if (!verifyTable(result, known)) {
-		printf("Generated results don't match known values\n");
-	}
-    
+    // Run this once and compare the result to the known table
+    create_etc1_to_dxt1_6_conversion_table();
+    if (!verifyTable(result, known)) {
+        printf("Generated results don't match known values\n");
+    }
+
     // Perform multiple runs and take the best time
     unsigned best = UINT32_MAX;
     for (int n = 10; n > 0; n--) {
-    	unsigned time = millis();
-    	create_etc1_to_dxt1_6_conversion_table();
-    	time = millis() - time;
-    	if (time < best) {
-    		best = time;
-    	}
+        unsigned time = millis();
+        create_etc1_to_dxt1_6_conversion_table();
+        time = millis() - time;
+        if (time < best) {
+            best = time;
+        }
     }
-    
+
     printf("Best run took %dms\n", best);
     return 0;
 }
